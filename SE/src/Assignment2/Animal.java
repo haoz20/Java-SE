e22f46e548cd889cc5736b3772e0d793f97059ca
haoz20 <zhaonianhao2005@gmail.com>
2024-08-20 23:02:56 +0700

    add SE

---
+++ SE/src/Assignment2/Animal.java
@@ -0,0 +1,18 @@
+package Assignment2;
+
+public class Animal {
+    String name;
+    int age;
+    String species;
+    static int totalNumber = 0;
+
+
+
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Species: " + species +"\n");
+    }
+
+
+}
