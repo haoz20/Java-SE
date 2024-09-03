c64282d58de2665ba9f82779a51a5686ef0afdf9
haoz20 <zhaonianhao2005@gmail.com>
2024-09-03 23:57:52 +0700

    readding

--- SalaryManagementSystem/src/com/example/app/Testing.java
+++ SalaryManagementSystem/src/com/example/app/Testing.java
@@ -12,35 +12,27 @@ import java.io.InputStreamReader;
 public class Testing {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//    public static void main(String[] args) throws IOException {
-//        EmployeeRegistrationService service = new EmployeeRegistrationService();
-//        System.out.print("Enter Employee Type: ");
-//        String type = br.readLine();
-//
-//        if (type.equalsIgnoreCase("contract")) {
-//            EmployeeRegistrationService contractService = new ContractEmployeeRegistrationService();
-//
-//        } else if (type.equalsIgnoreCase("fulltime")) {
-//            EmployeeRegistrationService fullTimeService = new FullTimeEmployeeRegistrationService();
-//
-//        } else if (type.equalsIgnoreCase("parttime")) {
-//            EmployeeRegistrationService partTimeService = new ContractEmployeeRegistrationService();
-//        }
-//
-//        service.getEmployeeInfo();
-//
-//        for (int i=0; i < Employee.getCount(); i++) {
-//            service.employeeDB[i].displayDetails();
-//        }
-//
-//
-//    }
+    public static void main(String[] args) throws IOException {
+        EmployeeRegistrationService service = new EmployeeRegistrationService();
+        System.out.print("Enter Employee Type: ");
+        String type = br.readLine();
 
-    public static void main(String[] args) {
-        Employee parttime = new PartTimeEmployee("Swan", 0, 3, 1000);
-        System.out.println(parttime.calculateSalary());
-        System.out.println(parttime.getCount());
+        if (type.equalsIgnoreCase("contract")) {
+            EmployeeRegistrationService contractService = new ContractEmployeeRegistrationService();
+
+        } else if (type.equalsIgnoreCase("fulltime")) {
+            EmployeeRegistrationService fullTimeService = new FullTimeEmployeeRegistrationService();
+
+        } else if (type.equalsIgnoreCase("parttime")) {
+            EmployeeRegistrationService partTimeService = new ContractEmployeeRegistrationService();
+        }
+
+        service.getEmployeeInfo();
+
+        for (int i=0; i < Employee.getCount(); i++) {
+            service.employeeDB[i].displayDetails();
+        }
+
     }
-
 
 }
